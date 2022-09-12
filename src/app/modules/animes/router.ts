f8bb0106f9a224fd30f24f8a1270edf1e2e6ebdc
jeferscoder <jeferscoder@gmail.com>
2022-09-12 09:52:27 -0300

    ACL/animes/genres

--- src/app/modules/animes/router.ts
+++ src/app/modules/animes/router.ts
@@ -1,13 +1,38 @@
+import { can, is } from '@app/middlewares/acl';
+import authenticated from '@app/middlewares/authenticated';
 import { Router } from 'express';
 import multer from 'multer';
 import * as controller from './animes.controller';
 import * as validator from './animes.validator';
 const router = Router();
 
-router.post('/', multer().single('cover'), validator.save, controller.save);
+router.post(
+  '/',
+  authenticated,
+  is(['admin', 'editor']),
+  can(['create']),
+  multer().single('cover'),
+  validator.save,
+  controller.save,
+);
 router.get('/', controller.findAll);
 router.get('/:id', validator.params, controller.findOne);
-router.patch('/:id', validator.params, validator.update, controller.update);
-router.delete('/:id', validator.params, controller.destroy);
+router.patch(
+  '/:id',
+  authenticated,
+  is(['admin', 'editor']),
+  can(['edit']),
+  validator.params,
+  validator.update,
+  controller.update,
+);
+router.delete(
+  '/:id',
+  authenticated,
+  is(['admin']),
+  can(['delete']),
+  validator.params,
+  controller.destroy,
+);
 
 export const animes = (routers: Router) => routers.use('/animes', router);
--- src/app/modules/genres/router.ts
+++ src/app/modules/genres/router.ts
@@ -1,12 +1,36 @@
+import { can, is } from '@app/middlewares/acl';
+import authenticated from '@app/middlewares/authenticated';
 import { Router } from 'express';
 import * as controller from './genres.controller';
 import * as validator from './genres.validator';
 const router = Router();
 
-router.post('/', validator.save, controller.save);
+router.post(
+  '/',
+  authenticated,
+  is(['admin', 'editor']),
+  can(['create']),
+  validator.save,
+  controller.save,
+);
 router.get('/', controller.findAll);
 router.get('/:id', validator.params, controller.findOne);
-router.patch('/:id', validator.params, validator.update, controller.update);
-router.delete('/:id', validator.params, controller.destroy);
+router.patch(
+  '/:id',
+  authenticated,
+  is(['admin', 'editor']),
+  can(['edit']),
+  validator.params,
+  validator.update,
+  controller.update,
+);
+router.delete(
+  '/:id',
+  authenticated,
+  is(['admin']),
+  can(['delete']),
+  validator.params,
+  controller.destroy,
+);
 
 export const genres = (routers: Router) => routers.use('/genres', router);
